c3ad8a3d68a09bd51e3459579b16ee3045589008
Wim <temmermanwim@gmail.com>
2020-08-06 23:26:49 +0200

    Update, proper vectorisation

--- main.jl
+++ main.jl
@@ -1,7 +1,7 @@
-using LinearAlgebra, Plots, Distributions, Random
+using LinearAlgebra, Plots, Distributions, Random, ColorSchemes
 
 f(x, y) = begin
-    -(3x + y ^ 2) * abs(sin(x) + cos(y))
+    (3x + y ^ 2) * abs(sin(x) + cos(y))
 end
 
 function generate_val(dom, h)
@@ -18,6 +18,7 @@ dom_init = [5, 10, 4, 8]
 a = generate_val(dom_init, 0.5)
 contour(a[1], a[2], a[5])
 
+#Generate m random points in domain
 function generate_rand(X, Y, m)
     rng = MersenneTwister(1234)
     randx = rand!(rng, zeros(m), X)
@@ -25,8 +26,21 @@ function generate_rand(X, Y, m)
     return hcat(randx, randy)'
 end
 
+#Rotate points around point with max fitness value (Xc)
 function rotate(θ, x)
     return [cos(θ) -sin(θ); sin(θ) cos(θ)]*x
+end
+
+function find_Xc(x)
+    result = -1e3
+    index = []
+    for i = 1:length(x[1,:])
+        if f(x[1, i], x[2,i]) > result
+            result = f(x[1, i], x[2,i])
+            index = x[:,i]
+        end
+    end
+    return index
 end
 
 #Dom: domain
@@ -35,41 +49,35 @@ end
 #θ : rotation angle
 #r : radius
 function SOA(dom, h, m, θ, r, n_iter)
+    #Generate m random points from domain, store in matrix A
     rng = MersenneTwister(1234)
     X, Y, Z = generate_val(dom, h)[3:5]
     X_rand = rand!(rng, zeros(m), X)
     Y_rand = rand!(rng, zeros(m), Y)
-    result = hcat(X_rand, Y_rand)'#Row 1: Xval, Row2: Yval
-    Xc = result[:,find_Xc(result)]
+    A = hcat(X_rand, Y_rand)' # Xval = A[1,:], Yval = A[2,:]
+    #Find Xc, critical point with highest fitness value
+    Xc = find_Xc(A)
+
     for i = 1:n_iter
-        for j = m-1:-1:0
-            x = r*rotate(θ,result[:,end-j]) - r*rotate(θ, Xc) + I(2)*Xc 
-            result = hcat(result,x)
-        end 
-        Xc = result[:, end - 5 + find_Xc(result[:, end-4:end])]
+        B = Array{Float64}(undef, 2, 0)
+
+        #Generate next m points
+        B = r*rotate(θ, A[:, end - m+1 : end]) .- r*rotate(θ, Xc) .+ Xc
+
+        #Add new points to A
+        A = hcat(A, B)
+
+        #From last m points, find new Xc
+        Xc = find_Xc(B)
+
     end
-    return result
+    contour(a[1], a[2], a[5])
+    scatter!(A[1,1:m:end], A[2,1:m:end])
+    scatter!(A[1,2:m:end], A[2,2:m:end])
+    scatter!(A[1,3:m:end], A[2,3:m:end])
+    savefig("SOA.pdf")
+    
+    return println("Optimimum at: ", A[:,end])
 end
 
-result = SOA(dom_init, 0.1, 5, π/9, 0.80, 30)
-result_x = result[1,:]
-result_y = result[2,:]
-contour(a[1], a[2], a[5])
-scatter!(result_x[1:5:end], result_y[1:5:end])
-savefig("SOA.pdf")
-scatter!(result_x[2:5:end], result_y[2:5:end])
-scatter!(result_x[3:5:end], result_y[3:5:end])
-scatter!(result_x[4:5:end], result_y[4:5:end])
-scatter!(result_x[5:5:end], result_y[5:5:end])
-
-function find_Xc(x)
-    result = 1e3
-    index = []
-    for i = 1:1:length(x[1,:])
-        if f(x[1, i], x[2,i]) < result
-            result = f(x[1, i], x[2,i])
-            index = push!(index, i)
-        end
-    end
-    return index[end]
-end
+result = SOA(dom_init, 0.1, 3, π/6, 0.87, 25)
